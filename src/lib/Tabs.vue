53dad2a28ccb4710ac00c49e078553897dac6917
TsaiJie <mengjietsai@163.com>
2022-02-03 14:16:21 +0800

    Tabs的基本样式

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -8,16 +8,51 @@ defaultSlots?.forEach(tag => {
     if (tag.type !== Tab) {
         throw new Error('Tabs 的子组件必须是Tab');
     }
-    
 });
 </script>
 
 <template>
-<div>
-	<div v-for="c in defaultSlots" :key="c">{{c.props?.title}}</div>
-	<component v-for="c in defaultSlots" :is="c" :key="c"></component>
+<div class="tsai-tabs">
+	<div class="tsai-tabs-nav">
+		<div class="tsai-tabs-nav-item" v-for="c in defaultSlots" :key="c">
+			{{ c.props?.title }}
+		</div>
+	</div>
+	<div class="tsai-tabs-content">
+		<component
+			class="tsai-tabs-content-item"
+			v-for="c in defaultSlots"
+			:is="c"
+			:key="c"
+		></component>
+	</div>
 </div>
-
 </template>
 
-<style scoped></style>
+<style lang="less">
+@blue: #40a9ff;
+@color: #333;
+@border-color: #d9d9d9;
+.tsai-tabs {
+    &-nav {
+        display: flex;
+        color: @color;
+        border-bottom: 1px solid @border-color;
+        &-item {
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+            &:first-child {
+                margin-left: 0;
+            }
+            &.selected {
+                color: @blue;
+            }
+        }
+    }
+    &-content {
+        padding: 8px 0;
+    }
+
+}
+</style>
