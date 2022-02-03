5a59065dd3eb9a766695a9fa0bdbd86d0e85ec39
TsaiJie <mengjietsai@163.com>
2022-02-03 15:34:02 +0800

    Tabs 支持selected

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,12 +1,13 @@
 <script lang="ts" setup>
 import Tabs from '@/lib/Tabs.vue';
 import Tab from '@/lib/Tab.vue';
-
+import { ref } from 'vue';
+const selected = ref('导航1');
 </script>
 
 <template>
 <div>
-	<Tabs>
+	<Tabs v-model:selected="selected">
 		<Tab title="导航1">内容1</Tab>
 		<Tab title="导航2">内容2</Tab>
 		<Tab title="导航3">内容3</Tab>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,14 @@
 <script lang="ts" setup>
-import { useSlots } from 'vue';
+import { computed, toRefs, useSlots, watch } from 'vue';
 import Tab from './Tab.vue';
+interface TabsProps {
+    selected: string;
+}
+const props = withDefaults(defineProps<TabsProps>(), {
+    selected: '',
+});
+const { selected } = toRefs(props);
+const emits = defineEmits(['update:selected']);
 //获取slots
 const defaultSlots = useSlots().default?.();
 //判断子组件的类型
@@ -9,22 +17,33 @@ defaultSlots?.forEach(tag => {
         throw new Error('Tabs 的子组件必须是Tab');
     }
 });
+const current = computed(
+    () => defaultSlots?.filter(item => item.props?.title === selected.value)[0]
+);
+const handClick = (title: string) => {
+    emits('update:selected', title);
+};
 </script>
 
 <template>
 <div class="tsai-tabs">
 	<div class="tsai-tabs-nav">
-		<div class="tsai-tabs-nav-item" v-for="c in defaultSlots" :key="c">
-			{{ c.props?.title }}
+		<div
+			class="tsai-tabs-nav-item"
+			:class="{ selected: item.props?.title === selected }"
+			v-for="item in defaultSlots"
+			:key="item"
+			@click="handClick(item.props?.title)"
+		>
+			{{ item.props?.title }}
 		</div>
 	</div>
 	<div class="tsai-tabs-content">
 		<component
 			class="tsai-tabs-content-item"
-			v-for="c in defaultSlots"
-			:is="c"
-			:key="c"
-		></component>
+			:is="current"
+			:key="current"
+		/>
 	</div>
 </div>
 </template>
@@ -53,6 +72,5 @@ defaultSlots?.forEach(tag => {
     &-content {
         padding: 8px 0;
     }
-
 }
 </style>
