dc32eec6936f20a9ca1b19323565665dfa0dd47a
TsaiJie <mengjietsai@163.com>
2022-02-05 19:25:33 +0800

    引入图标svg

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from '@/router';
 import '@/tsai.less';
 import '@/index.less';
 import App from '@/App.vue';
-
+import '@/lib/svg';
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
