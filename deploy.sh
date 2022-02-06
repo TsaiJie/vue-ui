0bf56504fd594e5e6312855925625bcb059ac2ba
TsaiJie <mengjietsai@163.com>
2022-02-06 13:07:11 +0800

    打包配置vite

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-rm -rf dist
+rm -rf dist_website
 yarn build
-cd dist
+cd dist_website
 git init
 git add .
-git commit -m "update dist"
+git commit -m "update dist_website"
 git branch -M main
 git remote add origin git@github.com:TsaiJie/vue-ui-website.git
 git push -f -u origin main
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -8,8 +8,36 @@ import path from 'path';
 import md from './plugin/md';
 import demoCode from './plugin/demoCode';
 // https://vitejs.dev/config
-
-export default defineConfig({
+const lib = defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve('src'),
+        },
+    },
+    // 打包配置
+    build: {
+        lib: {
+            // 入口文件
+            entry: path.resolve(__dirname, 'src/lib/index.ts'),
+            // 设置名字
+            name: 'tsai-vue-ui',
+            // 打包后的名字
+            fileName: format => `lib/tsai-vue-ui.${format}.js`,
+        },
+        outDir: 'dist_lib',
+        sourcemap: true,
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue',
+                },
+            },
+        },
+    },
+});
+const project = defineConfig({
     base: './',
     plugins: [vue(), md(), demoCode()],
     resolve: {
@@ -19,8 +47,16 @@ export default defineConfig({
     },
     // 打包配置
     build: {
-        lib: {
-            entry: path.resolve(__dirname, 'scr/lib/index.ts')
-        }
+        outDir: 'dist_website'
     }
+
 });
+
+export default ({ mode }) => {
+    const url = loadEnv(mode, process.cwd()).VITE_BASEURL;
+    if (url === 'lib') {
+        return lib;
+    } else {
+        return project;
+    }
+};
